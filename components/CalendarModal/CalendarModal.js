ff717f7dc18987afa6d8cb57cc18f9d6654696e2
Jaakko Kauppila <76628873+Kaubbila@users.noreply.github.com>
2024-04-07 15:27:16 +0300

    ToDo Forms ready

--- components/CalendarModal/CalendarModal.js
+++ components/CalendarModal/CalendarModal.js
@@ -1,34 +1,59 @@
 import { View, Text, Modal, StyleSheet, Button } from 'react-native'
 import React, { useState } from 'react'
 import DateTimePicker from '@react-native-community/datetimepicker';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const CalendarModal = ({calendarModalVisible, setCalendarModalVisible}) => {
-    const [date, setDate] = useState('')
-    const [show, setShow] = useState(false)
+    const [date, setDate] = useState(new Date()) //Tulee todomodaalista sit
+
+    const onChange = (e, selectedDate) => {
+      console.log(selectedDate);
+      console.log(e)
+      if (e.type === "dismissed") { 
+        setCalendarModalVisible(false); 
+    } else {
+        setDate(selectedDate);
+        setCalendarModalVisible(false);
+    }
+      setCalendarModalVisible(false)
+      console.log("Calendar Modal Visible: ", calendarModalVisible);
+    }
+
     return (
-    <Modal
+    <SafeAreaView>
+      <Modal
       animationType="none"
-      transparent={false}
+      transparent={true}
       visible={calendarModalVisible}
       onRequestClose={() => {
-        setCalendarModalVisible(!calendarModalVisible);
+        setCalendarModalVisible(false);
       }}
-    >
-        <View style={styles2.calendar}>
-            <Text >Hello</Text>
-            <Button title="Close" onPress={() => setCalendarModalVisible(!calendarModalVisible)} />
+      >
+        <View >
+            <DateTimePicker 
+            value={new Date()}
+            mode="date"
+            is24Hour={true}
+            locale="fi-FI"
+            timeZoneName={'Europe/Helsinki'}
+            dateFormat="day month year"
+            onChange={onChange}
+          />
+          <Button title="Close" onPress={() => setCalendarModalVisible(false)} />
         </View>
-    </Modal>
+      </Modal>
+    </SafeAreaView>
+    
   )
 }
 
-const styles2 = StyleSheet.create({
+/* const styles2 = StyleSheet.create({
     calendar: {
       flex: 1,
       backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'center'
     }
-  })
+  }) */
 
 export default CalendarModal;
--- components/ToDoModal/ToDoModal.js
+++ components/ToDoModal/ToDoModal.js
@@ -3,26 +3,52 @@ import { View, Button, Modal, Text, SafeAreaView, TextInput, Pressable, Touchabl
 import { useTheme } from "../../hooks/ThemeContext";
 import getDynamicStyles from './ToDoModal.styles';
 import { useForm, Controller } from 'react-hook-form';
-import CalendarModal from '../CalendarModal/CalendarModal';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 const ToDoModal = ({ todoModalVisible, setToDoModalVisible }) => {
 
   const { theme } = useTheme();
   const styles = getDynamicStyles(theme);
-  const [calendarModalVisible, setCalendarModalVisible] = useState(false);
+  const [calendarVisible, setCalendarVisible] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(new Date())
+  const [dateText, setDateText] = useState('')
+
+  const toDoSchema = yup.object().shape({
+    title: yup.string().required('Title is required'),
+    difficulty: yup.string().required('Difficulty is required'),
+    date: yup.string().required('Date is required'),
+  });
   const {
     control,
     handleSubmit,
     formState: { errors },
+    setValue
   } = useForm({
+    resolver: yupResolver(toDoSchema),
     defaultValues: {
       title: '',
       notes: '',
       difficulty: '',
-      date: ''
+      dueDate: ''
 
     },
   })
+  
+  const calendarAction = (e, selectedDate) => {
+    if (e.type === "dismissed") { 
+      setCalendarVisible(false); 
+    } else {
+      setCalendarVisible(false);
+      setSelectedDate(selectedDate);
+      let tempDate = new Date(selectedDate);
+      let choppedDate = tempDate.getDate() + '/' + (tempDate.getMonth() +1) + '/' + tempDate.getFullYear();
+      setDateText(choppedDate);
+      setValue('dueDate', choppedDate);
+    }
+  }
+    
   const onSubmit = (data) => console.log(data)
 
   return (
@@ -40,94 +66,84 @@ const ToDoModal = ({ todoModalVisible, setToDoModalVisible }) => {
         </View>
         <View style={styles.top}>
           <Button title="Back" onPress={() => setToDoModalVisible(!todoModalVisible)} />
-          <Pressable 
+          <TouchableOpacity
             style={styles.text}
             onPress={handleSubmit(onSubmit)}>
               <Text>CREATE</Text>
-            </Pressable>
+          </TouchableOpacity>
         </View>
         <View style={styles.main}>
           <View style={styles.cont}>
             <Text style={styles.text}>TITLE</Text>
             <Controller
-            control={control}
-            rules={{
-              required: true,
-            }}
-            render={({ field: { onChange, value } }) => (
-              <TextInput
-                placeholder=''
-                onChangeText={onChange}
-                value={value}
-              />
-            )}
-            name="title"
+              control={control}
+              rules={{
+                required: true,
+              }}
+              render={({ field: { onChange, value } }) => (
+                <View>
+                  <TextInput
+                  placeholder=''
+                  onChangeText={onChange}
+                  value={value}
+                />
+                {errors.title && (
+                  <Text style={styles2.errorText}>{errors.title.message}</Text>
+                )}
+                </View>
+              )}
+              name="title"
             />
           </View>
           <View style={styles.cont}>
             <Text style={styles.text}>NOTES</Text>
             <Controller
-            control={control}
-            rules={{
-              required: true,
-            }}
-            render={({ field: { onChange, value } }) => (
+              control={control}
+              rules={{
+                required: false,
+              }}
+              render={({ field: { onChange, value } }) => (
               <TextInput
                 placeholder=''
                 onChangeText={onChange}
                 value={value}
               />
-            )}
-            name="notes"
+              )}
+              name="notes"
             />
           </View>
           <Text style={styles.text}>DIFFICULTY</Text>
-          <View style={styles.difficultyOption}>
-            <View style={styles.difficultyBox}>
+              {errors.difficulty && (
+                <Text style={styles2.errorText}>{errors.difficulty.message}</Text>
+              )}
               <Controller
                 control={control}
                 rules={{
                   required: true,
                 }}
                 render={({ field: { onChange, value } }) => (
-                <TouchableOpacity
-                  style={styles.difficultyBox}
-                  onPress={() => {
-                    onChange('easy')
-                  }}
-                >
-                <Text>EASY</Text>
-                </TouchableOpacity>
-                )}
-                name="difficulty"
-              />
-            </View>
-            <View style={styles.difficultyBox}>
-              <Controller
-                  control={control}
-                  rules={{
-                    required: true,
-                  }}
-                  render={({ field: { onChange, value } }) => (
-                  <TouchableOpacity
+                <View style={styles.difficultyOption}>
+                  <View style={styles.difficultyBox}>
+                    <TouchableOpacity
                     style={styles.difficultyBox}
                     onPress={() => {
-                      onChange('medium')
+                      onChange('easy')
                     }}
-                  >
-                  <Text>MEDIUM</Text>
-                  </TouchableOpacity>
-                  )}
-                  name="difficulty"
-              />
-            </View>
-            <View style={styles.difficultyBox}>
-              <Controller
-                    control={control}
-                    rules={{
-                      required: true,
-                    }}
-                    render={({ field: { onChange, value } }) => (
+                    >
+                    <Text>EASY</Text>
+                    </TouchableOpacity>
+                  </View>
+                  <View style={styles.difficultyBox}>
+                    <TouchableOpacity
+                      style={styles.difficultyBox}
+                      onPress={() => {
+                        onChange('medium')
+                      }}
+                    >
+                    <Text>MEDIUM</Text>
+                    </TouchableOpacity>
+                  </View>
+                  <View style={styles.difficultyBox}>
                     <TouchableOpacity
                       style={styles.difficultyBox}
                       onPress={() => {
@@ -136,23 +152,44 @@ const ToDoModal = ({ todoModalVisible, setToDoModalVisible }) => {
                     >
                     <Text>HARD</Text>
                     </TouchableOpacity>
-                    )}
-                    name="difficulty"
-                />
-            </View>
-          </View>
+                  </View>
+                </View>
+                )}
+                name="difficulty"
+              />
+              
           <View style={styles.cont}>
-            <TouchableOpacity onPress={() => setCalendarModalVisible(true)}>
-              <Text style={styles.text}>Due Date:</Text>
-            </TouchableOpacity> 
-                <CalendarModal
-                calendarModalVisible={calendarModalVisible}
-                setCalendarModalVisible={setCalendarModalVisible}
-                />               
+            <TouchableOpacity onPress={() => setCalendarVisible(true)}>
+              <Text style={styles.text}>Due Date: </Text>
+            </TouchableOpacity>
+            {calendarVisible && (
+              <DateTimePicker 
+                value={selectedDate}
+                mode='date'
+                is24Hour={true}
+                locale="fi-FI"
+                timeZoneName={'Europe/Helsinki'}
+                display='default'
+                onChange={calendarAction}
+              />
+              )}
+              {errors.date && !dateText ? (
+                <Text style={styles2.errorText}>{errors.date.message}</Text>
+              ) : (
+                <Text>{dateText}</Text>
+              )}
+              
           </View>
         </View>
       </View>
     </Modal>
   );
 };
+
+const styles2 =  StyleSheet.create ({
+    errorText: {
+      color: '#AE0000'
+    }
+});
+
 export default ToDoModal;
